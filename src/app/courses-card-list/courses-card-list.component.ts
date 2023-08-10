5fca2a51b7f4be5364395617c558b584299c3e61
Asmaa Majid <majid.asmaa2001@gmail.com>
2023-08-10 19:08:48 +0100

    angular material Dialog

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {Course} from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
 
 @Component({
     selector: 'courses-card-list',
@@ -12,7 +13,7 @@ export class CoursesCardListComponent implements OnInit {
     @Input()
     courses: Course[];
 
-    constructor() {
+    constructor(private dialog:MatDialog) {
     }
 
     ngOnInit() {
@@ -20,6 +21,14 @@ export class CoursesCardListComponent implements OnInit {
     }
 
     editCourse(course:Course) {
+      openEditCourseDialog(this.dialog, course)
+          .pipe(
+            // filter(val => !!val )
+          )
+          .subscribe(
+            val => console.log("new course value", val)
+
+          );
 
 
     }
